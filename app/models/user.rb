545667bee414f0e9989dfae02f8d0e7c6a5cb912
Horino Yutaro <suzy.city16@gmail.com>
2020-12-27 15:17:55 +0900

    [add] userモデルに投稿者か否かを判別するメソッドを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def has_written?(post)
+    posts.exists?(id: post.id)
+  end
 end
