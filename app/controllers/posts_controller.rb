795cce1be1f7d5774033859734697df8c0dd2bd0
Kento Yoshioka <>
2020-12-30 11:02:15 +0900

    [update] new, create アクションを追加 ログインユーザーのみアクセスできるように設定
    new, create へ投稿機能を実装し、before_action :authenticate_user! でアクセス制限追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,22 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :show]
 
   def index
+
   end
-  
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.create!(post_params)
+    render :new
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
