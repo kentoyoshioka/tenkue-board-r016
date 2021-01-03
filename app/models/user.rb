3970c1262f0c94352c241a5c0f21bcdb13107ece
Horino Yutaro <suzy.city16@gmail.com>
2021-01-03 19:03:55 +0900

    [update] Userモデルのリレーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :posts, dependent: :destroy
+
   def has_written?(post)
     posts.exists?(id: post.id)
   end
