3965cd29e8ba5f9fb224e27c2caabc319dc47e85
EC2 Default User <ec2-user@ip-172-31-22-107.ap-northeast-1.compute.internal>
2021-01-04 10:12:39 +0000

    add indexメソッド修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-    
+  before_action :login_required
+  
+  
+  
+  private
+   def login_required
+       redirect_to   unless currentt_user
+   end
   
 end
