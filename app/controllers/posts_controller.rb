48d085f063b1c631b7b0d0a36d56164376ad7f0f
EC2 Default User <ec2-user@ip-172-31-22-107.ap-northeast-1.compute.internal>
2021-01-04 11:50:36 +0000

    編集と削除メソッド追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,25 @@ class PostsController < ApplicationController
     @posts = Post.all.include?(:user)
   end
   
+  def edit
+    @post = Post.find_by(id:params[:id])
+  end
+  
+  def update
+    if @post.update(post_params)
+      redirect_to topic_path
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to topic_path
+  end
+  
+  
+  praivate
+   def post_params
+     params.require(:post).permit(:title, :content, :name)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   end
 
   root to: 'posts#index'
+  
+  resources :posts
 end
