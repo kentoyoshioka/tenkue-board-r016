74d543777d1a146d6e95b6f0cae4304174a32abf
EC2 Default User <ec2-user@ip-172-31-22-107.ap-northeast-1.compute.internal>
2021-01-04 10:26:06 +0000

    未ログインでも閲覧できる機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :login_required
+  before_action :login_required, only: [:index, :show]
   
   
   
   private
    def login_required
-       redirect_to   unless currentt_user
+       redirect_to users_guest_sign_in_path  unless current_user
    end
   
 end
