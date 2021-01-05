520bad76fb492e98ab1e301f431babb11c66d64f
EC2 Default User <ec2-user@ip-172-31-22-107.ap-northeast-1.compute.internal>
2021-01-05 22:39:53 +0000

    update postテーブルのuser_idカラムをuserに変更

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(version: 2021_01_04_120708) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.text "name"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
