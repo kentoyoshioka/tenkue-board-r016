1881060944e724a634cf09369dc1927f1026c719
Horino Yutaro <suzy.city16@gmail.com>
2021-01-06 22:31:47 +0900

    [add] postsコントローラのプライベートメソッドにset_postを定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,4 +26,9 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:content)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
