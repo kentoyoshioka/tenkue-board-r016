89d0eb2e58ea8d3f78209963afd32598efed5ff8
Horino Yutaro <suzy.city16@gmail.com>
2021-01-10 23:57:11 +0900

    [add] likesコントローラのcreateアクションの実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,9 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
+
+  def create
+    post = Post.find(params[:post_id])
+    post.likes.create!(user_id: current_user.id)
+    redirect_to post_path(post)
+  end
 end
