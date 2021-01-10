8170b11a14f648f86a6a20c74188a611cb4d7279
Horino Yutaro <suzy.city16@gmail.com>
2021-01-10 23:50:51 +0900

    [add] LikeモデルをPostモデルとUserモデルと紐付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   def has_written?(post)
     posts.exists?(id: post.id)
