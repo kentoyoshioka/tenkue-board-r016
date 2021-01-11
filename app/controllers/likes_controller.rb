358e75e86673976718392905d02840a70fa39a71
Horino Yutaro <suzy.city16@gmail.com>
2021-01-11 15:52:32 +0900

    [fix] likesコントローラのリファクタリング

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,20 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_post, only: [:create, :destroy]
 
   def create
-    post = Post.find(params[:post_id])
     post.likes.create!(user_id: current_user.id)
     redirect_to post_path(post)
   end
 
   def destroy
-    post = Post.find(params[:post_id])
     like = post.likes.find_by!(user_id: current_user.id)
     like.destroy!
     redirect_to post_path(post)
+  end
+
+  private
+  def set_post
+    post = Post.find(params[:post_id])
   end
 end
