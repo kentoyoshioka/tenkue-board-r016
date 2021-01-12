12f45581fb857d814715f1217e4a78663f79cfe8
Kento Yoshioka <>
2021-01-13 01:33:23 +0900

    [update] before_actionの削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,4 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!
-
   def create
     # @post = Post.find(params[:post_id])
     # @comments = @post.comments.order(created_at: :desc)
